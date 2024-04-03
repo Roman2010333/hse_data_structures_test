0cf6f6d439703ac4cec98ce8d73b9a9bcc9093a9
Roman2010333 <116678401+Roman2010333@users.noreply.github.com>
2024-04-03 19:59:51 +0300

    Update simplemath.py

--- main/simplemath.py
+++ main/simplemath.py
@@ -1,3 +1,13 @@
 
 def f_me(x):
-    return 0 # changeme
+    if isinstance(x, str):
+        return None
+    if x is None:
+        return None
+    if x == float("inf") or x == float("-inf"):
+        return None
+
+    if x == 0:
+        return 10
+
+    return x * 25 + 10
